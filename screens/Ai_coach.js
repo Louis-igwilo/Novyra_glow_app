c81c8ff83b32a44d4413d430447c68e4a913f9d4
Louis Igwilo <oslouis31@gmail.com>
2026-04-17 07:26:46 +0100

    feat: onboarding setup and initial project structure

---
+++ screens/Ai_coach.js
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function Ai_coach(){
+    return(
+        <View>
+            <Text>Ai coach screen</Text>
+        </View>
+    )
+}
---
+++ screens/Journal_insights.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Journal_insights(){
+    return(
+        <View>
+            <Text>Journal and Insights screens</Text>
+        </View>
+    );
+}
